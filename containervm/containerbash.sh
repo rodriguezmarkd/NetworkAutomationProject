407801ec4505e0f8ae61ff6a533c529bbb9d9f45
rodriguezmarkd <rodriguezmarkd@gmail.com>
2023-10-21 15:13:12 +0000

    Adding VM

--- containervm/containerbash.sh
+++ containervm/containerbash.sh
@@ -51,7 +51,50 @@ sudo mkdir /var/log/qemu
 
 MAC=$(printf "aa:a3:a3:%02x:%02x:%02x\n" $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)))
 
-sudo ip netns exec whost echo $KEY1
+echo $KEY1
 
-sudo ip netns exec whost cd /var/kvm/images
+cd /var/kvm/images
 
+echo 'instance-id: sdn-1-test
+local-hostname:sdn-1-test = 1' | sudo tee meta-data-1.yaml
+
+echo 'instance-id: sdn-2-test
+local-hostname:sdn-2-test = 1' | sudo tee meta-data-2.yaml
+
+cloud-localds cloud-init-1.iso user-data.yaml meta-data-1.yaml --network-config=net-config-1.yaml
+
+cloud-localds cloud-init-2.iso user-data.yaml meta-data-2.yaml --network-config=net-config-2.yaml
+
+sudo /sbin/iptables -t nat -A POSTROUTING -o ens3 -j MASQUERADE
+
+sudo /sbin/iptables -A FORWARD -i ens3 -o br0 -m state  --state RELATED,ESTABLISHED -j ACCEPT
+
+sudo /sbin/iptables -A FORWARD -i br0 -o ens3 -j ACCEPT
+
+mkdir -p /var/log/qemu/
+
+sudo /usr/bin/qemu-system-x86_64 \
+   -enable-kvm \
+   -drive file=/var/kvm/images/bionic-server-cloudimg-amd64.img,if=virtio \
+   -cdrom /var/kvm/images/cloud-init-1.iso \
+   -display curses \
+   -nographic \
+   -smp cpus=1 \
+   -m 1G \
+   -net nic,netdev=tap1,macaddr=$MAC1 \
+   -netdev bridge,id=tap1,br=br0\
+   -d int \
+   -D /var/log/qemu/qemu-1.log    
+
+sudo /usr/bin/qemu-system-x86_64 \
+   -enable-kvm \
+   -drive file=/var/kvm/images/bionic-server-cloudimg-amd64.img,if=virtio \
+   -cdrom /var/kvm/images/cloud-init-2.iso \
+   -display curses \
+   -nographic \
+   -smp cpus=1 \
+   -m 1G \
+   -net nic,netdev=tap1,macaddr=$MAC1 \
+   -netdev bridge,id=tap1,br=br0\
+   -d int \
+   -D /var/log/qemu/qemu-2.log    
